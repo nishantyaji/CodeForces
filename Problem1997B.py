aeec04d717835ace2940a966e863cd7a0b770807
nishantyaji <nishant.yaji@gmail.com>
2024-08-12 00:40:49 +0530

    Problem 1997B  - merge 2 contiguous if blocks

--- Problem1997B.py
+++ Problem1997B.py
@@ -13,8 +13,7 @@ for t in tests:
     res = 0
     for i in range(1, len(t[0]) - 1):
         for j in range(0, 2):
-            if t[j][i] == ".":
-                if t[j][i - 1] == "." and t[j][i + 1] == "." and t[j ^ 1][i] == "." and t[j ^ 1][i - 1] == "x" and \
-                        t[j ^ 1][i + 1] == "x":
-                    res += 1
+            if (t[j][i] == "." and t[j][i - 1] == "." and t[j][i + 1] == "." and t[j ^ 1][i] == "."
+                    and t[j ^ 1][i - 1] == "x" and t[j ^ 1][i + 1] == "x"):
+                res += 1
     print(res)

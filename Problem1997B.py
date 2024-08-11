9fca59b225474780c4aadda66fc4625235f957ec
nishantyaji <nishant.yaji@gmail.com>
2024-08-11 10:52:58 +0530

    Solution to Contest 1997 Problem B

---
+++ Problem1997B.py
@@ -0,0 +1,20 @@
+# https://codeforces.com/problemset/problem/1997/B
+
+num_tests = int(input())
+tests = []
+for n_t in range(num_tests):
+    l = input()
+    s = [input(), input()]
+    tests.append(s)
+
+# tests = [[".......x", ".x.xx..."], ["..", ".."], ["xxx", "xxx"], ["..x.x.x.x", "x.......x"]]
+
+for t in tests:
+    res = 0
+    for i in range(1, len(t[0]) - 1):
+        for j in range(0, 2):
+            if t[j][i] == ".":
+                if t[j][i - 1] == "." and t[j][i + 1] == "." and t[j ^ 1][i] == "." and t[j ^ 1][i - 1] == "x" and \
+                        t[j ^ 1][i + 1] == "x":
+                    res += 1
+    print(res)

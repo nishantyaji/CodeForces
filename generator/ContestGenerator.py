9b3d4f2024680060a3e5b4c15699d977ba9f05f4
nishantyaji <nishant.yaji@gmail.com>
2024-09-02 01:36:10 +0530

    Commit the contest generator scripts i.e. running the script will create the template code for the contest that should help us switch between console based and file based input seamlessly.

---
+++ generator/ContestGenerator.py
@@ -0,0 +1,24 @@
+import copy
+import os
+
+
+def create_contest():
+    contest = "Problem2009"
+    problems = ["A", "B", "C", "D", "E", "F", "G"]
+
+    os.mkdir("../contest/" + contest)
+    with open("template.txt", "r") as file:
+        file_content = file.read()
+
+    filenames = list(map(lambda x: contest + x, problems))
+    for fn in filenames:
+        file_content_copy = copy.deepcopy(file_content)
+        file_content_copy = file_content_copy.replace("zzz.zzz", fn + ".txt")
+        with open("../contest/" + contest + "/" + fn + ".txt", "w") as f:
+            pass
+        with open("../contest/" + contest + "/" + fn + ".py", "w") as f:
+            f.write(file_content_copy)
+
+
+if __name__ == '__main__':
+    create_contest()

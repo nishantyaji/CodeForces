99c5196b0fdf7c4b6221a474a0632bac5b3b07d4
nishantyaji <nishant.yaji@gmail.com>
2024-11-29 20:02:33 +0530

    Solution to the Contest 2035 Problem D (CodeForces Global Round 27)

---
+++ Problem2035D.py
@@ -0,0 +1,67 @@
+import math
+import platform
+
+in_fn = input
+op_fn = print
+
+input_file = platform.node() and platform.node().startswith("LAPTOP-")
+if input_file:
+    filename = "contest/Problem2035/Problem2035D.txt"
+    f = open(filename, "r")
+    in_fn = f.readline
+
+
+    def check_file(args):
+        exp = f.readline().strip()
+        if str(args) == exp:
+            print("Pass")
+        else:
+            print("Fail. Expected:", exp, "Got", args)
+
+
+    op_fn = check_file
+
+
+def read_int() -> int:
+    return int(in_fn().strip())
+
+
+def read_int_arr(sep=" ") -> list[int]:
+    return list(map(int, in_fn().strip().split(sep)))
+
+
+num_tests = read_int()
+tests = []
+for n_t in range(num_tests):
+    _ = read_int()
+    arr = read_int_arr()
+    tests.append(arr)
+
+
+def freduce(n: int):
+    this_pw = 0
+    if n % 2 == 0:
+        while n % 2 == 0:
+            this_pw += 1
+            n = n // 2
+    return n, this_pw
+
+
+mod_base = 1000000007
+for arr in tests:
+
+    stack = []
+    total = 0
+    res = []
+    for a in arr:
+        x_n, local_cum_2 = freduce(a)
+        while stack and math.log2(x_n) + (local_cum_2 + stack[-1][1]) > math.log2(stack[-1][0]) + stack[-1][1]:
+            total = (total + stack[-1][0] - stack[-1][0] * pow(2, stack[-1][1], mod_base)) % mod_base
+            local_cum_2 += stack[-1][1]
+            stack.pop()
+
+        stack.append((x_n, local_cum_2))
+        total = (total + x_n * pow(2, local_cum_2, mod_base)) % mod_base
+        local_cum_2 = 0
+        res.append(total)
+    op_fn(" ".join(list(map(str, res))))

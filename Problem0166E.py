1e6e466eb60739c5aee6cc3734c950704d90db30
nishant <nishant.yaji@gmail.com>
2026-01-26 10:05:51 +0530

    Solution to contest 166 question E

--- Problem0166E.py
+++ Problem0166E.py
@@ -1,5 +1,3 @@
-import functools
-
 in_fn = input
 op_fn = print
 
@@ -8,21 +6,31 @@ def read_int() -> int:
     return int(in_fn().strip())
 
 
-@functools.cache
-def dp_fn(x: int):
-    if x <= 2:
-        return 0
-    elif x == 3:
-        return 3
-    elif x == 4:
-        return 9
-    else:
-        res = 3 * pow(2, x - 3)
-        for i in range(1, x - 3):
-            res += (3 * pow(2, i - 1)) * dp_fn(x - 1 - i)
-        return res
+base = 1000000007
+
+
+# Strings of the form D***** are of 3^(n-1) in number
+# But to close the loop on the last move to D we have to discount
+# in 3^(n-1) the answers that end with D in penultimate position i.e. dp[n-1]
+# Therefore dp[n] = 3^(n-1)-dp[n-1]
+# recursively dp[n] = 3^(n-1) - 3^(n-2) + ...
+
+def sum_odd_pow_3(limit_pow: int):
+    pow9 = (limit_pow - 1) // 2
+    val = 3 * (pow(9, pow9 + 1, base) - 1) * pow(8, -1, base)
+    return val % base
+
+
+def sum_even_pow_3(limit_pow: int):
+    pow9 = (limit_pow - 2) // 2
+    val = 9 * (pow(9, pow9 + 1, base) - 1) * pow(8, -1, base)
+    return val % base
 
 
 n = read_int()
-n += 1
-op_fn(dp_fn(n))
+
+if n % 2 == 0:
+    res = (sum_odd_pow_3(n - 1) - sum_even_pow_3(n - 2)) % base
+else:
+    res = (sum_even_pow_3(n - 1) - sum_odd_pow_3(n - 2)) % base
+op_fn(res)
